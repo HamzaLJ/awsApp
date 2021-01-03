53627ec6bd6a342dd1b8fdd53210598b4a1d1617
Hamza L <hamza@example.com>
2021-01-03 22:55:04 +0100

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+//Comment the below line to get rid of the white screen when building for Android
+Amplify.configure(awsmobile);
 
 if (environment.production) {
   enableProdMode();
